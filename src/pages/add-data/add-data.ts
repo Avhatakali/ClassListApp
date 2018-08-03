d2ae11688ab8d2828cb8481cf74ce34545125e11
Avhatakali <avhatakali51@gmail.com>
2018-08-03 08:22:42 +0200

    commit

--- src/pages/add-data/add-data.ts
+++ src/pages/add-data/add-data.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
 import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { Toast } from '@ionic-native/toast';
 
@@ -13,9 +13,10 @@ export class AddDataPage {
   data = { name:"",  surname:"",identity:"", gender:"", email:"", address:"" };
 
   constructor(public navCtrl: NavController,
-    public navParams: NavParams,
-    private sqlite: SQLite,
-    private toast: Toast) {}
+        public navParams: NavParams,
+        public alertCtrl: AlertController,
+        private sqlite: SQLite,
+        private toast: Toast) {}
 
   saveData() {
     this.sqlite.create({
@@ -25,28 +26,15 @@ export class AddDataPage {
       db.executeSql('INSERT INTO class_list VALUES(NULL,?,?,?,?,?,?)',[this.data.name,this.data.surname,this.data.identity,this.data.gender, this.data.email, this.data.address])
         .then(res => {
           console.log(res);
-          this.toast.show('Data saved', '5000', 'center').subscribe(
-            toast => {
-              this.navCtrl.popToRoot();
-            }
-          );
+
+          const alert = this.alertCtrl.create({
+            title: ' CONFIRMATION  !',
+            subTitle: 'class mate successfully added !',
+            buttons: ['OK']
+          });
+          alert.present();
         })
-        .catch(e => {
-          console.log(e);
-          this.toast.show(e, '5000', 'center').subscribe(
-            toast => {
-              console.log(toast);
-            }
-          );
-        });
-    }).catch(e => {
-      console.log(e);
-      this.toast.show(e, '5000', 'center').subscribe(
-        toast => {
-          console.log(toast);
-        }
-      );
-    });
+     })
   }
 
 }
